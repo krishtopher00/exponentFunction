daab4be82675125ef6b7ea413bef90875a3e9196
Krish Bhavnani <kbhavnani2018@pingry.org>
2016-03-09 23:36:39 -0500

    Added documentation

--- exponentFunction.py
+++ exponentFunction.py
@@ -1,5 +1,10 @@
+# Author: Krish Bhavnani
+# Date: March 9, 2016
+
 def power(base,exponent):
+    # To loop and multiply the base by itself a certain number of times
     counter = 0 
+    # Need a starting value of 1 to properly account for each multiplication
     output = 1
     while counter < exponent:
         output *= base
@@ -10,6 +15,7 @@ def power(base,exponent):
 
 base = int(raw_input("Enter a base for your power function: "))
 exponent = int(raw_input("Enter an exponent for your power function: "))
+# Anything to the 0 power is 1!
 if exponent != 0:
     final = power(base,exponent)
     print("{}^{} is {}".format(base,exponent,final))

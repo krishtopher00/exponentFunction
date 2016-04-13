d2c3f99d004e136c54843c2feff2f195acfd0222
Krish Bhavnani <kbhavnani2018@pingry.org>
2016-04-12 23:04:30 -0400

    Changed exponent function to be recursive

--- exponentFunction.py
+++ exponentFunction.py
@@ -1,23 +1,49 @@
-# Author: Krish Bhavnani
-# Date: March 9, 2016
-
 def power(base,exponent):
-    # To loop and multiply the base by itself a certain number of times
-    counter = 0 
-    # Need a starting value of 1 to properly account for each multiplication
-    output = 1
-    while counter < exponent:
-        output *= base
-        counter += 1
-    return output
+    """Takes a number and raises it to another power"""
+    if exponent == 0:
+        return 1
+    else:
+        final = base * power(base, exponent - 1)
+    return final
 
-#------Main Program-------
+def int_input(value):
+    """Uses try except to turn user input into an int"""
+    number = raw_input(value)
+    notInt = True
+    while notInt == True:
+        try:
+            number = int(number)
+        except ValueError:
+            number = raw_input("Sorry that is not a integer, try again: ")
+        else:
+            notInt = False
+    return number
+    
+def float_input(value): 
+    """Uses try except to turn user input into a float"""
+    number = raw_input(value)
+    notFloat = True
+    while notFloat == True:
+        try:
+            number = float(number)
+        except ValueError:
+            number = raw_input("Sorry that is not a number, try again: ")
+        else:
+            notFloat = False
+    return number     
+        
+#------Test Cases-------
 
-base = int(raw_input("Enter a base for your power function: "))
-exponent = int(raw_input("Enter an exponent for your power function: "))
-# Anything to the 0 power is 1!
-if exponent != 0:
-    final = power(base,exponent)
-    print("{}^{} is {}".format(base,exponent,final))
-else:
-    print("{}^{} is 1".format(base,exponent))
+print("2 to the 3rd power is: {}".format(power(2,3)))
+print("5 to the 4th power is: {}".format(power(5,4)))
+print("8 to the 0 power is: {}".format(power(8,0)))
+
+
+#-----Main Program------
+base = float_input("Enter the base of the number: ")
+exponent = int_input("Enter an integer for the exponent: ")
+
+
+print("{} to the {} power is: {}".format(base, exponent, power(base,exponent)))
+
+
